c99ebab6595c596211980032caa7831c89d0becc
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2024-11-13 19:18:20 +0100

    Update UncontrolledFormatStrings.java

--- UncontrolledFormatStrings.java
+++ UncontrolledFormatStrings.java
@@ -5,7 +5,7 @@ public class test1 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your message:");
         String userInput = scanner.nextLine();
-        System.out.println(userInput);
+        System.out.format(userInput);
         scanner.close();
     }
 }
