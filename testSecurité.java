706d39f26131c587da1867b02a580bf6de27a610
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2024-11-13 17:02:24 +0100

    Update testSecurité.java

--- testSecurité.java
+++ testSecurité.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 public class UncontrolledFormatStringExample {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
         System.out.println("Enter your message:");
         String userInput = scanner.nextLine();
-
-        // Vulnérabilité : l'entrée utilisateur est directement utilisée dans la chaîne de format
         System.out.println(String.format("User message: " + userInput));
-
         scanner.close();
     }
 }
