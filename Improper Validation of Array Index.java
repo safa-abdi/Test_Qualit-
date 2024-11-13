cdd361e2bedf10029cfa6d2d2bcfc233f61cc25b
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2024-11-13 19:01:34 +0100

    Update Improper Validation of Array Index.java

--- Improper Validation of Array Index.java
+++ Improper Validation of Array Index.java
@@ -1,8 +1,14 @@
-public class ArrayIndexExample {
+import java.util.Scanner;
+
+public class ArrayIndexSecurityExample {
 
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};  
-        int index = 6;  
-        System.out.println("Element at index " + index + ": " + numbers[index]);
+        int[] sensitiveData = {42, 56, 99, 123, 888};  
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an index to access the sensitive data: ");
+        int userIndex = scanner.nextInt();  
+
+        System.out.println("Sensitive Data at index " + userIndex + ": " + sensitiveData[userIndex]);
     }
 }
