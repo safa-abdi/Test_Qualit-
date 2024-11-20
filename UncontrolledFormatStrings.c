292f74440a465ec03208ea9fce104cc5c4adca09
Abdi Safa <80724587+safa-abdi@users.noreply.github.com>
2024-11-20 07:02:35 +0100

    Update UncontrolledFormatStrings.c

--- UncontrolledFormatStrings.c
+++ UncontrolledFormatStrings.c
@@ -2,14 +2,8 @@
 
 int main() {
     char userInput[256];
-
     printf("Enter your message: ");
-    // Utilisation de scanf sans contrôle sur le format, ce qui permet un format string vulnérable.
-    scanf("%s", userInput);
-
-    // Affichage direct de l'entrée de l'utilisateur sans échapper le formatage
-    // Cela est vulnérable aux attaques de type format string
-    printf(userInput);  // VULNÉRABILITÉ : Aucun contrôle sur l'entrée utilisateur
-
+    fgets(userInput, sizeof(userInput), stdin);
+    printf(userInput);  
     return 0;
 }
